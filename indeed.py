e73c6e308cb914257de4c287ddc034997341cc29
Juyong Hong <juyong3525@gmail.com>
2021-01-05 17:14:12 +0900

    Extracting Titles

--- indeed.py
+++ indeed.py
@@ -7,23 +7,24 @@ URL = f"https://kr.indeed.com/%EC%B7%A8%EC%97%85?q=python&limit={LIMIT}"
 
 def extract_indeed_pages():
     result = requests.get(URL)
-
     soup = BeautifulSoup(result.text, 'html.parser')
-
     pagination = soup.find("div", {"class": "pagination"})
-
     links = pagination.find_all('a')
     pages = []
     for link in links[:-1]:
         pages.append(int(link.string))
-
     max_page = pages[-1]
     return max_page
 
 
 def extract_indeed_jobs(last_page):
     jobs = []
-    for page in range(last_page):
-        result = requests.get(f"{URL}&start={page*LIMIT}")
-        print(result.status_code)
+    # for page in range(last_page):
+    result = requests.get(f"{URL}&start={0*LIMIT}")  # 0*LIMIT => page*LIMIT
+    soup = BeautifulSoup(result.text, 'html.parser')
+    cards = soup.find_all("div", {"class": "jobsearch-SerpJobCard"})
+    for card in cards:
+        title = card.find("h2", {"class": "title"}).find('a')["title"]
+        print(title)
+
     return jobs
