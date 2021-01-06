5cc36261a2a0f9b1abeedaf3c70bac3dbd9118b7
Juyong Hong <juyong3525@gmail.com>
2021-01-06 18:41:42 +0900

    Extracting Locations and Finishing up

--- indeed.py
+++ indeed.py
@@ -17,14 +17,36 @@ def extract_indeed_pages():
     return max_page
 
 
+def extract_job(html):
+    title = html.find("h2", {"class": "title"}).find("a")["title"]
+    company = html.find("span", {"class": "company"})
+    company_anchor = company.find("a")
+    if company_anchor is not None:
+        company = company_anchor.string.strip()
+    else:
+        company = company.string.strip()
+    location = html.find("div", {"class": "recJobLoc"})["data-rc-loc"]
+    # link = "https://kr.indeed.com" + \
+    #     html.find("h2", {"class": "title"}).find("a")["href"]
+    job_id = html["data-jk"]
+
+    return {
+        'title': title,
+        'company': company,
+        'location': location,
+        'link': f"https://kr.indeed.com/viewjob?jk={job_id}"
+    }
+
+
 def extract_indeed_jobs(last_page):
     jobs = []
-    # for page in range(last_page):
-    result = requests.get(f"{URL}&start={0*LIMIT}")  # 0*LIMIT => page*LIMIT
-    soup = BeautifulSoup(result.text, 'html.parser')
-    cards = soup.find_all("div", {"class": "jobsearch-SerpJobCard"})
-    for card in cards:
-        title = card.find("h2", {"class": "title"}).find('a')["title"]
-        print(title)
+    for page in range(last_page):
+        print(f"Scrapping page {page}")
 
+        result = requests.get(f"{URL}&start={page*LIMIT}")
+        soup = BeautifulSoup(result.text, 'html.parser')
+        cards = soup.find_all("div", {"class": "jobsearch-SerpJobCard"})
+        for card in cards:
+            job = extract_job(card)
+            jobs.append(job)
     return jobs
