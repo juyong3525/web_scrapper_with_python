34b96ff3d387a79822ea212734059155d977d365
Juyong Hong <juyong3525@gmail.com>
2021-01-06 19:04:15 +0900

    StackOverflow Pages

--- indeed.py
+++ indeed.py
@@ -5,7 +5,7 @@ LIMIT = 50
 URL = f"https://kr.indeed.com/%EC%B7%A8%EC%97%85?q=python&limit={LIMIT}"
 
 
-def extract_indeed_pages():
+def get_last_page():
     result = requests.get(URL)
     soup = BeautifulSoup(result.text, 'html.parser')
     pagination = soup.find("div", {"class": "pagination"})
@@ -20,11 +20,14 @@ def extract_indeed_pages():
 def extract_job(html):
     title = html.find("h2", {"class": "title"}).find("a")["title"]
     company = html.find("span", {"class": "company"})
-    company_anchor = company.find("a")
-    if company_anchor is not None:
-        company = company_anchor.string.strip()
+    if company:
+        company_anchor = company.find("a")
+        if company_anchor is not None:
+            company = company_anchor.string.strip()
+        else:
+            company = company.string.strip()
     else:
-        company = company.string.strip()
+        company = None
     location = html.find("div", {"class": "recJobLoc"})["data-rc-loc"]
     # link = "https://kr.indeed.com" + \
     #     html.find("h2", {"class": "title"}).find("a")["href"]
@@ -38,7 +41,7 @@ def extract_job(html):
     }
 
 
-def extract_indeed_jobs(last_page):
+def extract_jobs(last_page):
     jobs = []
     for page in range(last_page):
         print(f"Scrapping page {page}")
@@ -49,4 +52,10 @@ def extract_indeed_jobs(last_page):
         for card in cards:
             job = extract_job(card)
             jobs.append(job)
+    return jobs
+
+
+def get_jobs():
+    last_page = get_last_page()
+    jobs = extract_jobs(last_page)
     return jobs
