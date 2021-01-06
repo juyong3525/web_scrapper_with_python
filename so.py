e6a3d009f4909c1cf6223912469449021b26137e
Juyong Hong <juyong3525@gmail.com>
2021-01-06 20:18:51 +0900

    Saving to CSV

--- so.py
+++ so.py
@@ -21,26 +21,31 @@ def extract_job(html):
         company = company.get_text(strip=True)
     else:
         company = None
-
     if location != None:
         location = location.get_text(strip=True)
     else:
         location = None
+    job_id = html["data-jobid"]
 
-    return {'title': title, 'company': company, 'location': location}
+    return {
+        'title': title,
+        'company': company,
+        'location': location,
+        'link': f"https://stackoverflow.com/jobs/{job_id}"
+    }
 
 
 def extract_jobs(last_page):
     jobs = []
-    # for page in range(last_page):
-    #     result = requests.get(f"{URL}&pg={page+1}") 코멘트 나중에 해제
-    result = requests.get(URL)  # 나중에 코멘트하거나 삭제
-    soup = BeautifulSoup(result.text, 'html.parser')
-    cards = soup.find_all("div", {"class": "-job"})
-    for card in cards:
-        # print(card["data-jobid"])
-        job = extract_job(card)
-        jobs.append(job)
+    for page in range(last_page):
+        print(f"Scrapping SO: Page: {page}")
+
+        result = requests.get(f"{URL}&pg={page+1}")
+        soup = BeautifulSoup(result.text, 'html.parser')
+        cards = soup.find_all("div", {"class": "-job"})
+        for card in cards:
+            job = extract_job(card)
+            jobs.append(job)
     return jobs
 
 
